f17e9cf35de23bee44db2ea12797518d88497093
Leonardo Cervantes <LeoCerva29@gmail.com>
2024-06-18 16:51:25 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function changeTheme() {
-    var theme = document.getElementsByTagNameNS('link');
-    if (theme.getAttribute('href') == 'light.css') {
-        theme.setAttribute('href', 'dark.css');
-    } else {
-        theme.setAttribute('href', 'light.css');
-    }
-}
